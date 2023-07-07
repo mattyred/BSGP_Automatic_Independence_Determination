32302a7e13d61bd1839957ddfbddff6ed122b5b2
MATTIA ROSSO s294711 <s294711@studenti.polito.it>
2023-07-07 15:16:59 +0200

    Added histogram precision function

--- results/process_results.py
+++ results/process_results.py
@@ -26,17 +26,36 @@ def process_results(filepath=None, precise_kernel=0, d=6):
                 precisions_merged_var[i, j] = np.var(precisions_merged[i, j])
         return precisions_merged, precisions_merged_mean, precisions_merged_var, posterior_samples_kerlogvar
 
-def heatmap_precision(precisions_mean, precisions_var, fig_height=15, fig_width=5):
-    fig, ax = plt.subplots(1, 2, figsize=(fig_height, fig_width))
+def heatmap_precision(precisions_mean, precisions_var, annot=True, fig_height=7, fig_width=5):
+    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
     d = precisions_mean.shape[0]
     min, max = np.min(precisions_mean), np.max(precisions_mean)
-    h = sns.heatmap(precisions_mean, annot=True, fmt='.3f', annot_kws={"size": 8}, cmap='vlag', vmax=max, vmin=-max, center=0, linewidth=.5, ax=ax[0]) 
+    h = sns.heatmap(precisions_mean, annot=annot, fmt='.3f', annot_kws={"size": 8}, cmap='vlag', vmax=max, vmin=-max, center=0, linewidth=.5, ax=ax) 
     for i in range(d):
         for j in range(d):
             mean_text = f'{precisions_mean[i, j]:.3f}'
             variance_text = f'({precisions_var[i, j]:.3f})'
             text = f'{mean_text}\n' + f'{variance_text}'
             h.texts[i * d + j].set_text(text)
-    ax[0].set_title(r'$\Lambda$ - mean(var) over samples')
-    ax[1].set_title('RBF-ARD inv. squared lengthscales')
+    ax.set_title(r'$\Lambda_{i,j}$: mean(var) over samples')
+    plt.show()
+
+def histograms_precision(precisios_merged=None, lengthscales_merged=None, d=6, fig_width=10, fig_height=10, bins=100):
+    fig, axes = plt.subplots(d, d, figsize=(fig_width, fig_height))
+    LRBF_min, LRBF_max = np.min(np.min(precisios_merged)), np.max(np.max(precisios_merged))
+    if lengthscales_merged is not None:
+        ARD_min, ARD_max = np.min(np.min(lengthscales_merged)), np.max(np.max(lengthscales_merged))
+    else:
+        ARD_min, ARD_max = LRBF_min, LRBF_max
+    glob_min, glob_max = np.min([ARD_min, LRBF_min]), np.max([ARD_max, LRBF_max])
+    for i in range(d):
+        for j in range(d):
+            ax = axes[i, j]
+            sns.histplot(precisios_merged[i, j], ax=ax, bins=100, kde=True)
+            if i==j:
+                sns.histplot(lengthscales_merged[i, j], ax=ax, bins=bins, kde=True)
+            ax.set_xlim(-(glob_max+1e-3), glob_max+1e-3)
+            ax.set_xlabel('')
+            ax.set_ylabel('')
+    fig.tight_layout()
     plt.show()
